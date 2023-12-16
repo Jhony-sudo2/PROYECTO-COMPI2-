62d44e222d58313800a68c74dcb7d8a34106baaa
Jhony-sudo2 <jhonyfuentes202031288@cunoc.edu.gt>
2023-12-15 19:15:08 -0600

    solucionando varias PK Y FORANEAS

--- src/aplicacion/querys/CreateTable.py
+++ src/aplicacion/querys/CreateTable.py
@@ -21,10 +21,10 @@ class CreateTable:
         print('creando archivo XML en: ' + ruta)
         data = {atributo.nombre: [atributo.tipo] for atributo in self.atributos}
 
-        data2 = {'Primaria': [atributo.nombre] for atributo in self.atributos
+        data2 = {f'Primaria_{i}': [atributo.nombre ]for i, atributo in enumerate(self.atributos, start=1)
                  if atributo.condicion is not None and atributo.condicion == 2
                  }
-        data5 = {'NOTNULL': [atributo.nombre] for atributo in self.atributos
+        data5 = {f'NOTNULL_{i}': [atributo.nombre] for i, atributo in enumerate(self.atributos, start=1)
                  if atributo.condicion is not None and atributo.condicion == 1
                  }
 
@@ -32,14 +32,14 @@ class CreateTable:
 
                  for atributo in self.atributos if atributo.foranea is not None]
 
-        data4 = {'Foranea': data3}
-        if  bool(data2['Primaria']):
+        data4 = {f'Foranea_{i}': [valor] for valor in data3 for i, valor in enumerate(data3,start=1)}
+
+        if  data2:
             data.update(data2)
-        if  bool(data4['Foranea']):
+        if  data4:
             data.update(data4)
-        if bool(data5['NOTNULL']):
+        if data5:
             data.update(data5)
-        print(data)
         df = pd.DataFrame(data)
 
         xml_cadena = df.to_xml(index=False, root_name='Tabla', row_name='Estructura')
